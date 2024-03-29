fc8c815356d3237fd23fc44b08f58ecb50e9d14d
Gints Polis <polis.gints@gmail.com>
2024-03-29 10:37:14 +0200

    Small layout update

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,8 @@
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*, timer::TimerGroup};
+use esp_hal::{clock::ClockControl, Rng, embassy, peripherals::Peripherals, prelude::*, systimer, timer::TimerGroup};
 use esp_println::println;
-
-#[embassy_executor::task]
-async fn run() {
-    loop {
-        println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
-
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -32,5 +23,13 @@ async fn main(spawner: Spawner) {
     loop {
         println!("Bing!");
         Timer::after(Duration::from_millis(5_000)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn run() {
+    loop {
+        println!("Hello world from embassy using esp-hal-async!");
+        Timer::after(Duration::from_millis(1_000)).await;
     }
 }
